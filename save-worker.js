a5ebe0deafbae91082569b794fac29189b0deafa
SamTheBus <sammyenciso@gmail.com>
2026-09-05 13:54:45 -0700

    Update game: mobile resume, background saves and touch controls

---
+++ save-worker.js
@@ -0,0 +1,3 @@
+globalThis.__SAQ_SAVE_WORKER__=true;
+importScripts("./data-01.js?v=749f19a8ca8f8302","./data-02.js?v=749f19a8ca8f8302","./data-03.js?v=749f19a8ca8f8302","./data-04.js?v=749f19a8ca8f8302","./data-05.js?v=749f19a8ca8f8302","./data-06.js?v=749f19a8ca8f8302","./data-07.js?v=749f19a8ca8f8302","./data-08.js?v=749f19a8ca8f8302","./data-09.js?v=749f19a8ca8f8302","./data-10.js?v=749f19a8ca8f8302","./data-11.js?v=749f19a8ca8f8302","./data-12.js?v=749f19a8ca8f8302");
+import("./inflate.js?v=749f19a8ca8f8302");
