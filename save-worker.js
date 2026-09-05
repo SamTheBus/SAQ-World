63f41fad7b3e81106316874e9cdbc346a0713472
SamTheBus <sammyenciso@gmail.com>
2026-09-05 15:00:43 -0700

    Update game: fixed full-screen shell and portrait/landscape safe areas

--- save-worker.js
+++ save-worker.js
@@ -1,3 +1,3 @@
 globalThis.__SAQ_SAVE_WORKER__=true;
-importScripts("./data-01.js?v=07d6dfd7a1d2a1aa","./data-02.js?v=07d6dfd7a1d2a1aa","./data-03.js?v=07d6dfd7a1d2a1aa","./data-04.js?v=07d6dfd7a1d2a1aa","./data-05.js?v=07d6dfd7a1d2a1aa","./data-06.js?v=07d6dfd7a1d2a1aa","./data-07.js?v=07d6dfd7a1d2a1aa","./data-08.js?v=07d6dfd7a1d2a1aa","./data-09.js?v=07d6dfd7a1d2a1aa","./data-10.js?v=07d6dfd7a1d2a1aa","./data-11.js?v=07d6dfd7a1d2a1aa","./data-12.js?v=07d6dfd7a1d2a1aa");
-import("./inflate.js?v=07d6dfd7a1d2a1aa");
+importScripts("./data-01.js?v=807a436b58737f91","./data-02.js?v=807a436b58737f91","./data-03.js?v=807a436b58737f91","./data-04.js?v=807a436b58737f91","./data-05.js?v=807a436b58737f91","./data-06.js?v=807a436b58737f91","./data-07.js?v=807a436b58737f91","./data-08.js?v=807a436b58737f91","./data-09.js?v=807a436b58737f91","./data-10.js?v=807a436b58737f91","./data-11.js?v=807a436b58737f91","./data-12.js?v=807a436b58737f91");
+import("./inflate.js?v=807a436b58737f91");
