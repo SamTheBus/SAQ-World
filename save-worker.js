65eabf9ed5d5f1e47bbbc719dee831ffe5625a8d
SamTheBus <sammyenciso@gmail.com>
2026-09-05 17:20:17 -0700

    Update D1-R2 playtest with soft melee assist and top mobile HUD

--- save-worker.js
+++ save-worker.js
@@ -1,3 +1,3 @@
 globalThis.__SAQ_SAVE_WORKER__=true;
-importScripts("./data-01.js?v=7fda11dab811dea9","./data-02.js?v=7fda11dab811dea9","./data-03.js?v=7fda11dab811dea9","./data-04.js?v=7fda11dab811dea9","./data-05.js?v=7fda11dab811dea9","./data-06.js?v=7fda11dab811dea9","./data-07.js?v=7fda11dab811dea9","./data-08.js?v=7fda11dab811dea9","./data-09.js?v=7fda11dab811dea9","./data-10.js?v=7fda11dab811dea9","./data-11.js?v=7fda11dab811dea9","./data-12.js?v=7fda11dab811dea9","./data-13.js?v=7fda11dab811dea9");
-import("./inflate.js?v=7fda11dab811dea9");
+importScripts("./data-01.js?v=05698f21d183c736","./data-02.js?v=05698f21d183c736","./data-03.js?v=05698f21d183c736","./data-04.js?v=05698f21d183c736","./data-05.js?v=05698f21d183c736","./data-06.js?v=05698f21d183c736","./data-07.js?v=05698f21d183c736","./data-08.js?v=05698f21d183c736","./data-09.js?v=05698f21d183c736","./data-10.js?v=05698f21d183c736","./data-11.js?v=05698f21d183c736","./data-12.js?v=05698f21d183c736","./data-13.js?v=05698f21d183c736");
+import("./inflate.js?v=05698f21d183c736");
