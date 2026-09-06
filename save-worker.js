68c5788d5e12cce2c6888c4419e52c0c21368c15
SamTheBus <sammyenciso@gmail.com>
2026-09-05 18:26:05 -0700

    Publish WORLD.1G-X-D2 Stonewatch phone playtest

--- save-worker.js
+++ save-worker.js
@@ -1,3 +1,3 @@
 globalThis.__SAQ_SAVE_WORKER__=true;
-importScripts("./data-01.js?v=05698f21d183c736","./data-02.js?v=05698f21d183c736","./data-03.js?v=05698f21d183c736","./data-04.js?v=05698f21d183c736","./data-05.js?v=05698f21d183c736","./data-06.js?v=05698f21d183c736","./data-07.js?v=05698f21d183c736","./data-08.js?v=05698f21d183c736","./data-09.js?v=05698f21d183c736","./data-10.js?v=05698f21d183c736","./data-11.js?v=05698f21d183c736","./data-12.js?v=05698f21d183c736","./data-13.js?v=05698f21d183c736");
-import("./inflate.js?v=05698f21d183c736");
+importScripts("./data-01.js?v=50996fc9ef71ef44","./data-02.js?v=50996fc9ef71ef44","./data-03.js?v=50996fc9ef71ef44","./data-04.js?v=50996fc9ef71ef44","./data-05.js?v=50996fc9ef71ef44","./data-06.js?v=50996fc9ef71ef44","./data-07.js?v=50996fc9ef71ef44","./data-08.js?v=50996fc9ef71ef44","./data-09.js?v=50996fc9ef71ef44","./data-10.js?v=50996fc9ef71ef44","./data-11.js?v=50996fc9ef71ef44","./data-12.js?v=50996fc9ef71ef44","./data-13.js?v=50996fc9ef71ef44","./data-14.js?v=50996fc9ef71ef44");
+import("./inflate.js?v=50996fc9ef71ef44");
