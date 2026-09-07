8f68676222e1b23180af0bbd09368c8fd725c478
SamTheBus <sammyenciso@gmail.com>
2026-09-07 02:02:07 -0700

    Publish WORLD.1G-X-D4 manual loot, inventory and equipment
    Ship tested game-only release a5d312aee04cf6ac with automatic XP, personal manual pickups, persistent gear and equipment-driven weapon visuals.

--- save-worker.js
+++ save-worker.js
@@ -1,3 +1,3 @@
 globalThis.__SAQ_SAVE_WORKER__=true;
-importScripts("./data-01.js?v=464010e51968408c","./data-02.js?v=464010e51968408c","./data-03.js?v=464010e51968408c","./data-04.js?v=464010e51968408c","./data-05.js?v=464010e51968408c","./data-06.js?v=464010e51968408c","./data-07.js?v=464010e51968408c","./data-08.js?v=464010e51968408c","./data-09.js?v=464010e51968408c","./data-10.js?v=464010e51968408c","./data-11.js?v=464010e51968408c","./data-12.js?v=464010e51968408c","./data-13.js?v=464010e51968408c","./data-14.js?v=464010e51968408c");
-import("./inflate.js?v=464010e51968408c");
+importScripts("./data-01.js?v=a5d312aee04cf6ac","./data-02.js?v=a5d312aee04cf6ac","./data-03.js?v=a5d312aee04cf6ac","./data-04.js?v=a5d312aee04cf6ac","./data-05.js?v=a5d312aee04cf6ac","./data-06.js?v=a5d312aee04cf6ac","./data-07.js?v=a5d312aee04cf6ac","./data-08.js?v=a5d312aee04cf6ac","./data-09.js?v=a5d312aee04cf6ac","./data-10.js?v=a5d312aee04cf6ac","./data-11.js?v=a5d312aee04cf6ac","./data-12.js?v=a5d312aee04cf6ac","./data-13.js?v=a5d312aee04cf6ac","./data-14.js?v=a5d312aee04cf6ac","./data-15.js?v=a5d312aee04cf6ac");
+import("./inflate.js?v=a5d312aee04cf6ac");
