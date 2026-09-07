825ff51fdd08aa937cab73e151b6b3b0aea82430
SamTheBus <sammyenciso@gmail.com>
2026-09-06 23:44:19 -0700

    Publish D3-R death recovery and full healing on level-up

--- save-worker.js
+++ save-worker.js
@@ -1,3 +1,3 @@
 globalThis.__SAQ_SAVE_WORKER__=true;
-importScripts("./data-01.js?v=9fb484fc12358dba","./data-02.js?v=9fb484fc12358dba","./data-03.js?v=9fb484fc12358dba","./data-04.js?v=9fb484fc12358dba","./data-05.js?v=9fb484fc12358dba","./data-06.js?v=9fb484fc12358dba","./data-07.js?v=9fb484fc12358dba","./data-08.js?v=9fb484fc12358dba","./data-09.js?v=9fb484fc12358dba","./data-10.js?v=9fb484fc12358dba","./data-11.js?v=9fb484fc12358dba","./data-12.js?v=9fb484fc12358dba","./data-13.js?v=9fb484fc12358dba","./data-14.js?v=9fb484fc12358dba");
-import("./inflate.js?v=9fb484fc12358dba");
+importScripts("./data-01.js?v=464010e51968408c","./data-02.js?v=464010e51968408c","./data-03.js?v=464010e51968408c","./data-04.js?v=464010e51968408c","./data-05.js?v=464010e51968408c","./data-06.js?v=464010e51968408c","./data-07.js?v=464010e51968408c","./data-08.js?v=464010e51968408c","./data-09.js?v=464010e51968408c","./data-10.js?v=464010e51968408c","./data-11.js?v=464010e51968408c","./data-12.js?v=464010e51968408c","./data-13.js?v=464010e51968408c","./data-14.js?v=464010e51968408c");
+import("./inflate.js?v=464010e51968408c");
