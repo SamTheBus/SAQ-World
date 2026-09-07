feb9a7fe2da9f90142833f57782c1f21203c5dd8
SamTheBus <sammyenciso@gmail.com>
2026-09-06 22:19:26 -0700

    Publish D3 XP, levels and canonical player stats

--- save-worker.js
+++ save-worker.js
@@ -1,3 +1,3 @@
 globalThis.__SAQ_SAVE_WORKER__=true;
-importScripts("./data-01.js?v=1eecd814c13e5bbc","./data-02.js?v=1eecd814c13e5bbc","./data-03.js?v=1eecd814c13e5bbc","./data-04.js?v=1eecd814c13e5bbc","./data-05.js?v=1eecd814c13e5bbc","./data-06.js?v=1eecd814c13e5bbc","./data-07.js?v=1eecd814c13e5bbc","./data-08.js?v=1eecd814c13e5bbc","./data-09.js?v=1eecd814c13e5bbc","./data-10.js?v=1eecd814c13e5bbc","./data-11.js?v=1eecd814c13e5bbc","./data-12.js?v=1eecd814c13e5bbc","./data-13.js?v=1eecd814c13e5bbc","./data-14.js?v=1eecd814c13e5bbc");
-import("./inflate.js?v=1eecd814c13e5bbc");
+importScripts("./data-01.js?v=9fb484fc12358dba","./data-02.js?v=9fb484fc12358dba","./data-03.js?v=9fb484fc12358dba","./data-04.js?v=9fb484fc12358dba","./data-05.js?v=9fb484fc12358dba","./data-06.js?v=9fb484fc12358dba","./data-07.js?v=9fb484fc12358dba","./data-08.js?v=9fb484fc12358dba","./data-09.js?v=9fb484fc12358dba","./data-10.js?v=9fb484fc12358dba","./data-11.js?v=9fb484fc12358dba","./data-12.js?v=9fb484fc12358dba","./data-13.js?v=9fb484fc12358dba","./data-14.js?v=9fb484fc12358dba");
+import("./inflate.js?v=9fb484fc12358dba");
